8c23ff82a3429d4c4c122f648b07ee46266ac37a
Filip Dobrowolski <filip.dobrowolski@outlook.com>
2019-04-19 20:06:13 +0200

    DemoAppPredica.Web solution created. New template of core project.

---
+++ DemoAppPredica.Web/DemoAppPredica.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoAppPredica.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
