98beeb038922f6d35fc51cca5d952f8c7cbd0907
Filip Dobrowolski <filip.dobrowolski@outlook.com>
2019-04-19 23:10:38 +0200

    Enabled migrations in data library.

---
+++ DemoAppPredica.Web/DemoAppPredica.Data/Migrations/20190419210821_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoAppPredica.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Journeys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    DestinationCountry = table.Column<string>(nullable: true),
+                    Cost = table.Column<int>(nullable: false),
+                    IsValid = table.Column<bool>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Journeys", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Journeys");
+        }
+    }
+}
